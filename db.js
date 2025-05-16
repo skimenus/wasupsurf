2faaa43249b640fbbf1adcbc200eb82b146f7867
abuser77 <vladsoprano@gmail.com>
2025-05-16 11:51:11 +0000

    Создал db.js для подключения к Supabase Postgres

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 // db.js
 require('dotenv').config();
 const { Client } = require('pg');
+
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
+
 client.connect()
   .then(() => console.log('Connected to Postgres'))
   .catch(err => console.error('Postgres connection error', err));
+
 module.exports = client;
